91f543b4b9b40167f6a8c9be8c12b892e98cc1f0
Maya Saietz <mayasaietz@gmail.com>
2013-01-04 19:33:30 +0100

    Adds a general class for powerups, that all powerups and debuffs can inherit from. This class takes care of drawing and most of the initializing, but does not handle collisions.

--- powerup.py
+++ powerup.py
@@ -4,26 +4,47 @@ from gamelib.collider import BoxCollider
 from ball import Ball
 import pygame
 
-class HigherJump(GameObject):
-    def __init__(self,screen,pos):
+class PowerUp(GameObject):
+    def __init__(self,screen,pos,color):
         GameObject.__init__(self)
         self.position = pos
-        self.size = Vector2(50,20)
-        self.rect = pygame.Rect(pos.x-25,pos.y-20,50,20)
+        self.size = Vector2(50,10)
+        self.rect = pygame.Rect(pos.x-25,pos.y-self.size.y,50,self.size.y)
+        self.color = color
 
         self.collider = BoxCollider(self.rect)
 
         self.screen = screen
 
     def draw(self,pos):
-        pygame.draw.rect(self.screen,(200,200,255)
-                        ,pygame.Rect(pos.x-25,pos.y-20,self.size.x,self.size.y)
+        pygame.draw.rect(self.screen,self.color
+                        ,pygame.Rect(pos.x-25,pos.y-self.size.y,self.size.x,self.size.y)
                         ,3)
-        pygame.draw.rect(self.screen,(200,200,255)
+        pygame.draw.rect(self.screen,self.color
                         ,pygame.Rect(pos.x-10
-                                    ,pos.y-22
+                                    ,pos.y-(self.size.y+2)
                                     ,20,2),1)
+
+class HigherJump(PowerUp):
+    def __init__(self,screen,pos):
+        PowerUp.__init__(self,screen,pos,(100,100,255))
 
     def onCollision(self,col,obj):
         if isinstance(obj,Ball):
             obj.jumpspeed = 10
+
+class SuperJump(PowerUp):
+    def __init__(self,screen,pos):
+        PowerUp.__init__(self,screen,pos,(200,200,0))
+
+    def onCollision(self,col,obj):
+        if isinstance(obj,Ball):
+            obj.jumpspeed = 20
+
+class NoJump(PowerUp):
+    def __init__(self,screen,pos):
+        PowerUp.__init__(self,screen,pos,(255,100,100))
+
+    def onCollision(self,col,obj):
+        if isinstance(obj,Ball):
+            obj.jumpspeed = 0
